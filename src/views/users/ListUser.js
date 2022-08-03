46ef60ac465831fe881bc0d304161a3e0b3338f3
ThanhDoan <thanhdt2204@gmail.com>
2022-08-03 17:20:34 +0700

    Update feature get all users using hook

--- src/views/users/ListUser.js
+++ src/views/users/ListUser.js
@@ -1,101 +1,103 @@
-import React from 'react';
 import _ from 'lodash';
 
-class ListUser extends React.Component {
+const ListUser = (props) => {
 
-    handleChangePage = (pageNumber) => {
-        this.props.f_changePage(pageNumber);
+    const { numberOfElements, totalElements, totalPages } = props.userPage;
+    const userList = !_.isEmpty(props.userPage) ? props.userPage.content : [];
+    const currentPage = props.currentPage;
+
+    const handleChangePage = (pageNumber) => {
+        props.f_changePage(pageNumber);
     };
 
-    handleButtonDelete = (email) => {
-        this.props.f_buttonDelete(email);
+    const handleButtonDelete = (email) => {
+        props.f_buttonDelete(email);
     };
 
-    handleButtonUpdate = (email) => {
-        this.props.f_buttonUpdate(email);
+    const handleButtonUpdate = (email) => {
+        props.f_buttonUpdate(email);
     };
 
-    render() {
-        return (
-            <>
-                <table className="table table-striped table-hover">
-                    <thead>
-                        <tr>
-                            <th>Email</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+    return (
+        <>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        userList.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>
+                                        <a href="#editUserModal" onClick={() => handleButtonUpdate(item.email)} className="edit" data-toggle="modal">
+                                            <i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
+                                        </a>
+                                        <a href='#deleteUserModal' onClick={() => handleButtonDelete(item.email)} className="delete" data-toggle="modal">
+                                            <i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+            {userList.length > 0 &&
+                <div className="clearfix">
+                    <div className="hint-text">
+                        Showing&nbsp;
+                        <b>{numberOfElements}</b>&nbsp;
+                        out of&nbsp;
+                        <b>{totalElements}</b>&nbsp;
+                        entries
+                    </div>
+                    <ul className="pagination">
+                        {currentPage === 0 &&
+                            <li className="page-item disabled">
+                                <a href='# '>Previous</a>
+                            </li>
+                        }
+                        {currentPage > 0 &&
+                            <li onClick={() => handleChangePage(currentPage - 1)} className="page-item">
+                                <a href='# '>Previous</a>
+                            </li>
+                        }
+
                         {
-                            this.props.userList.map((item, index) => {
+                            _.range(0, totalPages, 1).map((item, index) => {
+                                const activeClass = currentPage === item ? 'active' : '';
                                 return (
-                                    <tr key={index}>
-                                        <td>{item.email}</td>
-                                        <td>{item.firstName}</td>
-                                        <td>{item.lastName}</td>
-                                        <td>
-                                            <a href="#editUserModal" onClick={() => this.handleButtonUpdate(item.email)} className="edit" data-toggle="modal">
-                                                <i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i>
-                                            </a>
-                                            <a href='#deleteUserModal' onClick={() => this.handleButtonDelete(item.email)} className="delete" data-toggle="modal">
-                                                <i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                )
+                                    <li onClick={() => handleChangePage(item)} key={index} className={`page-item ${activeClass}`}>
+                                        <a href='# ' className="page-link">{item + 1}</a>
+                                    </li>
+                                );
                             })
                         }
-                    </tbody>
-                </table>
-                {this.props.userList.length > 0 &&
-                    <div className="clearfix">
-                        <div className="hint-text">
-                            Showing&nbsp;
-                            <b>{this.props.numberOfElements}</b>&nbsp;
-                            out of&nbsp;
-                            <b>{this.props.totalElements}</b>&nbsp;
-                            entries
-                        </div>
-                        <ul className="pagination">
-                            {this.props.currentPage === 0 &&
-                                <li className="page-item disabled">
-                                    <a href='# '>Previous</a>
-                                </li>
-                            }
-                            {this.props.currentPage > 0 &&
-                                <li onClick={() => this.handleChangePage(this.props.currentPage - 1)} className="page-item">
-                                    <a href='# '>Previous</a>
-                                </li>
-                            }
+                        {currentPage === totalPages - 1 &&
+                            <li className="page-item disabled">
+                                <a href='# ' className="page-link">Next</a>
+                            </li>
+                        }
+                        {currentPage < totalPages - 1 &&
+                            <li onClick={() => handleChangePage(currentPage + 1)} className="page-item">
+                                <a href='# ' className="page-link">Next</a>
+                            </li>
+                        }
+                    </ul>
+                </div>
+            }
+        </>
+    );
 
-                            {
-                                _.range(0, this.props.totalPages, 1).map((item, index) => {
-                                    const activeClass = this.props.currentPage === item ? 'active' : '';
-                                    return (
-                                        <li onClick={() => this.handleChangePage(item)} key={index} className={`page-item ${activeClass}`}>
-                                            <a href='# ' className="page-link">{item + 1}</a>
-                                        </li>
-                                    );
-                                })
-                            }
-                            {this.props.currentPage === this.props.totalPages - 1 &&
-                                <li className="page-item disabled">
-                                    <a href='# ' className="page-link">Next</a>
-                                </li>
-                            }
-                            {this.props.currentPage < this.props.totalPages - 1 &&
-                                <li onClick={() => this.handleChangePage(this.props.currentPage + 1)} className="page-item">
-                                    <a href='# ' className="page-link">Next</a>
-                                </li>
-                            }
-                        </ul>
-                    </div>
-                }
-            </>
-        );
-    }
 }
 
 export default ListUser;
--- src/views/users/User.js
+++ src/views/users/User.js
@@ -1,135 +1,105 @@
-import React from 'react';
-import './User.scss';
-import UpdateUser from './UpdateUser';
+import { useEffect, useState } from 'react';
+import { withRouter } from "react-router";
+import { toast, ToastContainer } from 'react-toastify';
+import userApi from '../../services/userService';
+import { message, pagination } from '../../utils/constant';
 import DeleteUser from './DeleteUser';
 import ListUser from './ListUser';
-import userApi from '../../services/userService';
-import { withRouter } from "react-router";
-import { pagination } from '../../utils/constant';
-import { ToastContainer, toast } from 'react-toastify';
-import { message } from '../../utils/constant';
+import UpdateUser from './UpdateUser';
+import './User.scss';
 
-class User extends React.Component {
+const User = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentPage: 0,
-            userList: [],
-            user: {},
-            numberOfElements: 0,
-            totalElements: 0,
-            totalPages: 0,
-            email: '',
-            isOpenUpdateModal: false
-        }
-    }
+    const [currentPage, setCurrentPage] = useState(0);
+    const [email, setEmail] = useState('');
+    const [isOpenUpdateModal, setIsOpenUpdateModal] = useState(false);
+    const [userPage, setUserPage] = useState({});
 
-    getAllUsers = async (currentPage) => {
+    const getAllUsers = async (currentPage) => {
         let response = await userApi.getAllUsers({ page: currentPage, size: pagination.PAGE_SIZE });
         if (response.status === 200) {
-            this.setState({
-                userList: response.data.content,
-                numberOfElements: response.data.numberOfElements,
-                totalElements: response.data.totalElements,
-                totalPages: response.data.totalPages
-            })
+            setUserPage(response.data);
         }
     }
 
-    componentDidMount() {
-        this.getAllUsers(this.state.currentPage);
+    useEffect(() => {
+        getAllUsers(currentPage);
+    }, [currentPage]);
+
+    const handleAddNew = () => {
+        props.history.push("/user/new");
     }
 
-    handleAddNew = () => {
-        this.props.history.push("/user/new");
+    const f_changePage = (pageNumber) => {
+        setCurrentPage(pageNumber);
     }
 
-    f_changePage = (pageNumber) => {
-        this.setState({
-            currentPage: pageNumber
-        })
-        this.getAllUsers(pageNumber);
+    const f_buttonDelete = (email) => {
+        setEmail(email);
     }
 
-    f_buttonDelete = (email) => {
-        this.setState({
-            email: email,
-        })
-    }
-
-    f_delete = async () => {
-        await userApi.deleteUser(this.state.email).then((response) => {
+    const f_delete = async () => {
+        await userApi.deleteUser(email).then((response) => {
             if (response.status === 500) {
                 toast.error(message.INTERNAL_SERVER_ERROR);
             } else if (response.status === 400) {
                 toast.warn(response.status + ': ' + response.message);
             } else {
-                this.setState({
-                    currentPage: 0
-                })
-                this.getAllUsers(this.state.currentPage);
+                setCurrentPage(0);
+                getAllUsers(currentPage);
                 toast.success(message.DELETE_USER_SUCCESSFULLY);
             }
         });
     }
 
-    f_buttonUpdate = (email) => {
-        this.setState({
-            email: email,
-            isOpenUpdateModal: true
-        })
+    const f_buttonUpdate = (email) => {
+        setEmail(email);
+        setIsOpenUpdateModal(true);
     }
 
-    f_update = async (user) => {
+    const f_update = async (user) => {
         await userApi.updateUser(user).then((response) => {
             if (response.status === 500) {
                 toast.error(message.INTERNAL_SERVER_ERROR);
             } else if (response.status === 400) {
                 toast.warn(response.status + ': ' + response.message);
             } else {
-                this.setState({
-                    isOpenUpdateModal: false,
-                    currentPage: 0
-                })
-                this.getAllUsers(this.state.currentPage);
+                setCurrentPage(0);
+                setIsOpenUpdateModal(false);
+                getAllUsers(currentPage);
                 toast.success(message.UPDATE_USER_SUCCESSFULLY);
             }
         });
     }
 
-    render() {
-        return (
-            <>
-                <div className="container">
-                    <div className="table-wrapper">
-                        <div className="table-title">
-                            <div className="row">
-                                <div className="col-sm-6">
-                                    <h2>User Management</h2>
-                                </div>
-                                <div className="col-sm-6">
-                                    <a href='# ' onClick={this.handleAddNew} className="btn btn-success">
-                                        <i className="material-icons">&#xE147;</i>
-                                        <span>Add New</span>
-                                    </a>
-                                </div>
+    return (
+        <>
+            <div className="container">
+                <div className="table-wrapper">
+                    <div className="table-title">
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <h2>User Management</h2>
+                            </div>
+                            <div className="col-sm-6">
+                                <a href='# ' onClick={handleAddNew} className="btn btn-success">
+                                    <i className="material-icons">&#xE147;</i>
+                                    <span>Add New</span>
+                                </a>
                             </div>
                         </div>
-                        <ListUser userList={this.state.userList} numberOfElements={this.state.numberOfElements}
-                            totalElements={this.state.totalElements} totalPages={this.state.totalPages}
-                            currentPage={this.state.currentPage} f_changePage={this.f_changePage}
-                            f_buttonDelete={this.f_buttonDelete} f_buttonUpdate={this.f_buttonUpdate} />
                     </div>
+                    <ListUser userPage={userPage} currentPage={currentPage} f_changePage={f_changePage}
+                        f_buttonDelete={f_buttonDelete} f_buttonUpdate={f_buttonUpdate} />
                 </div>
-                {this.state.isOpenUpdateModal &&
-                    <UpdateUser email={this.state.email} f_update={this.f_update} />
-                }
-                <DeleteUser email={this.state.email} f_delete={this.f_delete} />
-                <ToastContainer />
-            </>
-        );
-    }
+            </div>
+            {isOpenUpdateModal &&
+                <UpdateUser email={email} f_update={f_update} />
+            }
+            <DeleteUser email={email} f_delete={f_delete} />
+            <ToastContainer />
+        </>
+    );
 
 }
 
