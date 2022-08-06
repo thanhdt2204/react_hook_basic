4934654465d19cb3a53f411d42587346d8a1c134
ThanhDoan <thanhdt2204@gmail.com>
2022-08-06 23:04:49 +0700

    Fix display page come back in not found page

--- src/views/error/NotFound.js
+++ src/views/error/NotFound.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { useHistory } from "react-router-dom";
+import StoreContext from "../../context/StoreContext";
 
 const NotFound = () => {
 
     const history = useHistory();
+    const { state } = useContext(StoreContext);
+    const pageDestination = state.isLoggedIn ? 'home page' : 'login page';
 
     const handleToHome = () => {
         history.push("/");
@@ -11,7 +15,7 @@ const NotFound = () => {
     return (
         <div style={{ color: "white", textAlign: "center", marginTop: "260px", fontWeight: "bold" }}>
             <div>Page not found</div>
-            <a href='# ' onClick={handleToHome}>Back to home page</a>
+            <a href='# ' onClick={handleToHome}>Back to {pageDestination}</a>
         </div>
     );
 
