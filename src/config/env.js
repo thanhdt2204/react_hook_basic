24269cc50ed1e54bce9baf46c2c53031cbaf715d
ThanhDoan <ThanhDoan@gmail.com>
2023-11-06 21:25:33 +0700

    Add api url production

--- src/config/env.js
+++ src/config/env.js
@@ -1,10 +1,13 @@
 const ENV = {
     LOCAL: {
         HOST_API: "http://localhost:5000/api"
+    },
+    PROD: {
+        HOST_API: "http://java-sample.ap-southeast-2.elasticbeanstalk.com/api"
     }
 }
 
-const config = ENV['LOCAL'];
+const config = ENV['LOCAL', 'PROD'];
 
 const getEnv = (name, defaultName) => config[name] || defaultName;
 
